a01dadf31d4d363037ded7523db68398e0ce4817
beatriz <bbarauna.beatriz@gmail.com>
2025-10-01 20:26:26 -0400

    Exercicio realizado na primeira aula de funcoes (29/10/25)

---
+++ quadrado.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int quadrado (int a) {
+	return a*a;
+}
+
+int main (){
+	//declaracao de variaveis
+	int n1, n2;
+	
+	//entrada de dados
+	printf("Entre com um numero: ");
+	scanf("%d", &n1);
+	
+	//chamada da funcao
+	n2 = quadrado(n1);
+	
+	//resultado
+	printf("O quadrado de %d eh: %d", n1, n2);
+	
+	return 0;
+}
